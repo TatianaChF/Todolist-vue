b8d11d615538fef2fd77500533977221d0b85242
TatianaChF <tatiana.chefox@gmail.com>
2025-03-30 13:48:42 +0300

    output of tasks as separate cards

---
+++ src/components/Task.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3>{{ props.taskData.title }}</h3>
+  <p>{{ props.taskData.description }}</p>
+  <input type="checkbox" name="completed" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps(["taskData"]);
+</script>
--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -3,18 +3,22 @@
   <div
       v-for="task in tasksStore.tasks"
       :key="task.id">
-    <p>{{task.title}}</p>
+    <Task :taskData="task" />
   </div>
 </template>
 
 <script setup lang="ts">
 import {useTasksStore} from "../store/tasks.ts";
 import {onMounted} from "vue";
+import Task from "./Task.vue";
 
 const tasksStore = useTasksStore();
 
+defineProps({
+  taskData: Object
+})
+
 onMounted(() => {
   tasksStore.getTasks();
-  console.log(tasksStore.tasks);
 })
 </script>
