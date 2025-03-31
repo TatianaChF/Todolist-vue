76f9a16d835ad79a9fb5c24963b11b9883c85ea1
TatianaChF <tatiana.chefox@gmail.com>
2025-03-31 20:33:00 +0300

    darkening when outputting a form

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="container">
+  <div :class="changeStyles">
     <h1>ToDo-list</h1>
     <button @click="isOpenForm = true"
             class="btn-add-task">
       Добавить задачу
     </button>
-    <TaskForm
-        v-show="isOpenForm"
-        @change-show-form="isOpenForm = false"/>
     <div
         v-for="task in tasksStore.tasks"
         :key="task.id">
       <Task :taskData="task" />
     </div>
   </div>
+  <TaskForm
+      v-show="isOpenForm"
+      @change-show-form="isOpenForm = false"/>
 </template>
 
 <script setup lang="ts">
 import {useTasksStore} from "../store/tasks.ts";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import Task from "./Task.vue";
 import TaskForm from "./TaskForm.vue";
 
 const isOpenForm = ref<boolean>(false);
 const tasksStore = useTasksStore();
+
+const changeStyles = computed(() => {
+  return isOpenForm.value
+      ? "container container-hidden-space"
+      : "container";
+});
 
 defineEmits(["changeShowForm"]);
 
