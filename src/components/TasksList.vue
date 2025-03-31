01f19d1b2b35d43c79f7a7cf87c31f5ba693a4f4
TatianaChF <tatiana.chefox@gmail.com>
2025-03-31 20:52:53 +0300

    change style

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,35 +1,29 @@
 <template>
-  <div :class="changeStyles">
+  <div class="container">
     <h1>ToDo-list</h1>
     <button @click="isOpenForm = true"
             class="btn-add-task">
       Добавить задачу
     </button>
+    <TaskForm
+        v-show="isOpenForm"
+        @change-show-form="isOpenForm = false"/>
     <div
         v-for="task in tasksStore.tasks"
         :key="task.id">
       <Task :taskData="task" />
     </div>
   </div>
-  <TaskForm
-      v-show="isOpenForm"
-      @change-show-form="isOpenForm = false"/>
 </template>
 
 <script setup lang="ts">
 import {useTasksStore} from "../store/tasks.ts";
-import {computed, onMounted, ref} from "vue";
+import {onMounted, ref} from "vue";
 import Task from "./Task.vue";
 import TaskForm from "./TaskForm.vue";
 
 const isOpenForm = ref<boolean>(false);
 const tasksStore = useTasksStore();
-
-const changeStyles = computed(() => {
-  return isOpenForm.value
-      ? "container container-hidden-space"
-      : "container";
-});
 
 defineEmits(["changeShowForm"]);
 
