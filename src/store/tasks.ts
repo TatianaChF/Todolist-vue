07ac216ecea76512a08d854e0abaf1d11f6d61d8
TatianaChF <tatiana.chefox@gmail.com>
2025-04-01 20:47:08 +0300

    adding filtering methods

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -1,5 +1,5 @@
 import {defineStore} from "pinia";
-import {ref, watch} from "vue";
+import {computed, ref, watch} from "vue";
 import { v4 as generateId } from 'uuid';
 
 export interface Task {
@@ -9,11 +9,13 @@ export interface Task {
     completed: boolean;
 }
 
+type FilterType = 'all' | 'completed' | 'uncompleted';
+
 export const LOCAL_STORAGE_KEY = "tasksStorage";
 
 export const useTasksStore = defineStore('tasksData', () => {
     let tasks = ref<Array<Task>>([]);
-    let isResponse = ref(true);
+    const currentFilter = ref<FilterType>('all');
 
     const loadFromLocalStorage = () => {
         const tasksLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
@@ -43,7 +45,6 @@ export const useTasksStore = defineStore('tasksData', () => {
             try {
                 const response = await fetch("https://67e90de9bdcaa2b7f5b8707c.mockapi.io/list/tasks");
                 tasks.value = await response.json() as Task[];
-                isResponse.value = false;
             } catch (error) {
                 console.log(error);
             }
@@ -80,6 +81,22 @@ export const useTasksStore = defineStore('tasksData', () => {
         );
     }
 
+    const filteredTasks = computed(() => {
+        switch(currentFilter.value) {
+            case 'completed':
+                return tasks.value.filter(task => task.completed);
+            case 'uncompleted':
+                return tasks.value.filter(task => !task.completed);
+            default:
+                return tasks.value;
+        }
+    });
+
+    const setFilter = (filter: FilterType) => {
+        currentFilter.value = filter;
+    };
+
     return {tasks, getTasks, addTask,
-        removeTask, changeCompleteTask, updateTask}
+        removeTask, changeCompleteTask, updateTask,
+        currentFilter, filteredTasks, setFilter}
 })
